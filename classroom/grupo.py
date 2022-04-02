b303a0f7927075c836e62c28b4ff457240be24eb
shirohigexe <99839298+shirohigexe@users.noreply.github.com>
2022-04-02 15:12:59 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,36 +1,42 @@
-
-from asignatura import Asignatura
-from asignatura import Asignatura
-
-
+from classroom.asignatura import Asignatura
 class Grupo:
 
-    grado = None
+    grado = "Grado 12" 
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes = None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
+    def listadoAsignaturas(self, **kwargs):
+        self._asignaturas = []
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x))
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
         if(lista is None):
-            lista.append(alumno)
-            self.listadoAlumnos = self.listadoAlumnos + lista
-        else:
+            #lista.append(alumno)
             self.listadoAlumnos = [alumno]
+        else:
+            lista = list(lista)
+            lista.append(alumno)
+            self.listadoAlumnos = lista
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
         cls.grado = nombre
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
+    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
+    def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
+
+#    @classmethod
+#    def asignarNombre(cls, nombre):
+#        cls.grado = nombre
+
+    def __str__(self):
+        return "Grupo de estudiantes: " + self._grupo
